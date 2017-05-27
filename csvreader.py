32d66542403c7f59d7a8cb87258a6f6edf582faf
Jakob Mathiszig-Lee <jakob@mathisziglee.co.uk>
2017-05-27 16:42:44 +0100

    started integrating model function with csv

--- csvreader.py
+++ csvreader.py
@@ -1,4 +1,5 @@
 import csv
+from patient_state import PatientState
 
 csvfile = "propofol.csv"
 
@@ -14,27 +15,35 @@ for row in csv.reader(read):
         #still on same patient, carry on
         print 'oldpatient'
     else:
-        pid = newid
+        #save mean error from this patient
+        meanerror = totalerror / totalmeasurements
+        #do something with this like save it to a file database?
+
         #new patient, reset compartments
-        print 'newpatient'
-    age = row[6]
-    weight = row[7]
-    height = row[8]
-    sex = row[9]
+        pid = newid
+        age = row[6]
+        weight = row[7]
+        height = row[8]
+        sex = row[9]
+
+        patient = PatientState(age, weight, height, sex)
+        totalmeasurements = 0
+        totalerror = 0
+
     mg = row[3]
     rate = row[4]
     cp = row[2]
     cp = float(cp)
+    time = float(mg) / float(rate)
 
     if cp == 0:
         #no plasma concentration so calculate and move on
-        time = float(mg) / float(rate)
-        print 'time:'
-        print time
+        patient.give_drug(mg)
+        patient.wait_time(time)
     else:
         #do a comparison with x1 and store it somewhere
-        print 'plasma concentration alert!'
-
-    print age
-    print mg
-    print cp
+        predcp = patient.x1
+        newerror = cp - pred_cp
+        rmserror = SQRT(newerror**)
+        totalmeasurements ++
+        totalerror = totalerror + rmserror
--- test.py
+++ test.py
@@ -1,16 +1,13 @@
 from patient_state import PatientState
+import re
 
 patient = PatientState(34, 46.3, 157.5, "f")
 print "Initial state: " + str(patient)
 
-patient.give_drug(92)
+patient.give_drug(92.60001)
 print "After giving drug: " + str(patient)
 
-patient.wait_time(2.11)
-print "After 1 second: " + str(patient)
-
-patient.wait_time(4.2)
-print "After another 1 minute: " + str(patient)
-
-patient.wait_time(8)
-print "After another 1 hour: " + str(patient)
+for t in range(120):
+    patient.wait_time(1)
+    print "After 2 mins: " + str(patient)
+    print patient.x1
