eb9b34d5da26f58846d2a5eb41198df8fe3d8623
Suzanne Hamilton <suzanne.hamilton@digital.cabinet-office.gov.uk>
2017-05-28 12:25:43 +0100

    Handle propofol amounts as infusions rather than boluses
    The original Schnider dataset was composed of boluses followed by an
    hour's wait, followed by an infusion.
    
    The data set we are using consists mostly of infusions. We know the rate
    of all of these, so we can model the dose given to the patient over
    time.

--- patient_solver.py
+++ patient_solver.py
@@ -4,21 +4,33 @@ def solve_for_patient(patient, params):
     print "Patient %s" % patient["id"]
 
     patient_model = PatientState(patient['age'], patient['weight'], patient['height'], patient['sex'], params)
-    patient_model.give_drug(patient['propofol_mg'])
-
-    previous_time_mins = 0
 
     total_lsq_error = 0
     total_measurements = 0
 
-    for measurement in patient['measurements']:
-        for t in range(int((measurement['time_mins'] - previous_time_mins) * 60)):
+    previous_time_mins = 0
+    current_dose_mg_per_sec = 0
+    infusion_seconds_remaining = 0
+
+    for event in patient['events']:
+        for t in range(int((event['time_mins'] - previous_time_mins) * 60)):
+            if infusion_seconds_remaining > 0:
+                patient_model.give_drug(current_dose_mg_per_sec)
+                infusion_seconds_remaining -= 1
+
             patient_model.wait_time(1)
 
-        predicted_cp = patient_model.x1
-        error = measurement['cp'] - predicted_cp
+            if event["type"] == "measurement":
+                predicted_cp = patient_model.x1
+                error = event['cp'] - predicted_cp
 
-        total_lsq_error += error ** 2
-        total_measurements += 1
+                total_lsq_error += error ** 2
+                total_measurements += 1
+            elif event["type"] == "start_infusion":
+                amount_mg = event["propofol_mg"]
+                current_dose_mg_per_sec = event["rate_mg_per_min"] / 60
+                infusion_seconds_remaining = amount_mg / current_dose_mg_per_sec
+            else:
+                raise ValueError("Unknown patient event type '%s'. Expected 'measurement' or 'start_infusion'" % event["type"])
 
     return total_lsq_error / total_measurements
