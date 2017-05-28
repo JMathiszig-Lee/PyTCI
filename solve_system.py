e8066984a41cbdeb6420ce00b064144357bb319b
Suzanne Hamilton <suzanne.hamilton@digital.cabinet-office.gov.uk>
2017-05-28 10:39:35 +0100

    Using dummy data, calculate the measurement error per patient

--- solve_system.py
+++ solve_system.py
@@ -19,7 +19,19 @@ def get_patients():
         'expected_result': 0,
         'weight': 70,
         'height': 180,
-        'sex': 'm'
+        'sex': 'm',
+        # TODO: Are there patients who receive multiple boluses when their cp is non-zero?
+        'propofol_mg': 92.60001,
+        'measurements': [
+            {
+                'time_mins': 2.11,
+                'predicted_cp': 3.62
+            },
+            {
+                'time_mins': 4.01,
+                'predicted_cp': 1.33
+            }
+        ]
     }]
 
 
@@ -38,17 +50,33 @@ def find_lsq_for_all_patients(params_vector):
     params = convert_vector_to_params_structure(params_vector)
     total_lsq = 0
     for patient in get_patients():
-        result = solve_for_patient(patient, params)
-        residual = result - patient['expected_result']
-        total_lsq += residual ** 2
+        # TODO: Use params rather than schnider_params
+        patient_error = solve_for_patient(patient, PatientState.schnider_params())
+        total_lsq += patient_error
+
+    return total_lsq
 
 
 def solve_for_patient(patient, params):
-    patient = PatientState(patient['age'], patient['weight'], patient['height'], patient['sex'], params)
-    patient.give_drug(92.60001)
-    for t in range(130):
-        patient.wait_time(1)
-    return patient.x1
+    patient_model = PatientState(patient['age'], patient['weight'], patient['height'], patient['sex'], params)
+    patient_model.give_drug(patient['propofol_mg'])
+
+    previous_time_mins = 0
+
+    totalerror = 0
+    total_measurements = 0
+
+    for measurement in patient['measurements']:
+        for t in range(int((measurement['time_mins'] - previous_time_mins) * 60)):
+            patient_model.wait_time(1)
+
+        actual_cp = patient_model.x1
+        error = actual_cp - measurement['predicted_cp']
+
+        totalerror += error
+        total_measurements += 1
+
+    return totalerror / total_measurements
 
 
 result = solve()
