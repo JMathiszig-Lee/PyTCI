6952efbf1a0686acb4294940bea0c88721ddf1f6
Jakob Mathiszig-Lee <jakob@mathisziglee.co.uk>
2019-02-15 22:34:53 +0000

    added most lean body weight formulas and have tests!

--- tests/test_lbm.py
+++ tests/test_lbm.py
@@ -1,4 +1,27 @@
 from ..weights import leanbodymass
 
+
 def test_james():
-    assert james(165, 90, f) == 52
+    assert round(leanbodymass.james(165, 90, "f")) == 52
+    assert round(leanbodymass.james(180, 60, "m")) == 52
+
+
+def test_bmi():
+    assert leanbodymass.bmi(180, 60) == 18.5
+
+
+def test_boer():
+    assert leanbodymass.boer(180, 60, "m") == 53.3
+
+
+def test_hume66():
+    assert leanbodymass.hume66(180, 60, "m") == 51.2
+
+
+def test_hume71():
+    pass
+
+
+def test_janmahasation():
+    assert leanbodymass.janmahasation(180, 60, "m") == 52.1
+    assert leanbodymass.janmahasation(165, 90, "f") == 49.5
--- weights/leanbodymass.py
+++ weights/leanbodymass.py
@@ -1,14 +1,74 @@
 def james(height: float, weight: float, sex: str) -> float:
     """ returns lean body mass as per james equations """
-    # TODO provide reference
+    """James, W. "Research on obesity: a report of the DHSS/MRC group" HM Stationery Office 1976"""
 
     if sex != "m" and sex != "f":
         raise ValueError(
             "Unknown sex '%s'. This algorithm can only handle 'm' and 'f'. :(" % sex
         )
 
-        # TODO: Use better equation to calculate lean body mass
-        if sex == "m":
-            return 1.1 * weight - 128 * ((weight / height) * (weight / height))
-        else:
-            return 1.07 * weight - 148 * ((weight / height) * (weight / height))
+    if sex == "m":
+        return 1.1 * weight - 128 * ((weight / height) * (weight / height))
+    else:
+        return 1.07 * weight - 148 * ((weight / height) * (weight / height))
+
+
+def boer(height: float, weight: float, sex: str) -> float:
+    """ returns lean body mass as per Boer equation """
+    """ Boer P. "Estimated lean body mass as an index for normalization of body fluid volumes in man." Am J Physiol 1984; 247: F632-5"""
+
+    if sex != "m" and sex != "f":
+        raise ValueError(
+            "Unknown sex '%s'. This algorithm can only handle 'm' and 'f'. :(" % sex
+        )
+
+    if sex == "m":
+        lbm = (0.407 * weight) + (0.267 * height) - 19.2
+    else:
+        lbm = (0.252 * weight) + (0.473 * height) - 48.3
+
+    return round(lbm, 1)
+
+
+def hume66(height: float, weight: float, sex: str) -> float:
+    """ returns lean body mass as per the 1966 Hume papre """
+    """ Hume, R "Prediction of lean body mass from height and weight.". J Clin Pathol. 1966 Jul; 19(4):389-91"""
+
+    if sex != "m" and sex != "f":
+        raise ValueError(
+            "Unknown sex '%s'. This algorithm can only handle 'm' and 'f'. :(" % sex
+        )
+
+    if sex == "m":
+        lbm = (0.32810 * weight) + (0.33929 * height) - 29.5336
+    else:
+        lbm = (0.29569 * weight) + (0.41813 * height) - 43.2933
+
+    return round(lbm, 1)
+
+
+def bmi(height: float, weight: float) -> float:
+    """calculates BMI"""
+    bmi = weight / ((height / 100) ** 2)
+    return round(bmi, 1)
+
+
+def janmahasation(height: float, weight: float, sex: str) -> float:
+    """ lean body mass as per Janmahasation / Han 2005 """
+
+    if sex != "m" and sex != "f":
+        raise ValueError(
+            "Unknown sex '%s'. This algorithm can only handle 'm' and 'f'. :(" % sex
+        )
+    bodymass = bmi(height, weight)
+
+    if sex == "m":
+        lbm = (9270 * weight) / (6680 + 216 * bodymass)
+    else:
+        lbm = (9270 * weight) / (8780 + 244 * bodymass)
+
+    return round(lbm, 1)
+
+
+if __name__ == "__main__":
+    print(james(165, 90, "f"))
