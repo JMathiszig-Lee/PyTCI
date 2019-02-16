d23577763eecee76c671dcd5da9917420e6406b7
Jakob Mathiszig-Lee <jakob@mathisziglee.co.uk>
2019-02-16 13:10:38 +0000

    expanding tests

--- tests/test_lbm.py
+++ tests/test_lbm.py
@@ -1,9 +1,13 @@
+import pytest
 from ..weights import leanbodymass
 
 
 def test_james():
     assert round(leanbodymass.james(165, 90, "f")) == 52
     assert round(leanbodymass.james(180, 60, "m")) == 52
+
+    with pytest.raises(ValueError):
+        leanbodymass.james(180, 60, "g")
 
 
 def test_bmi():
@@ -13,15 +17,24 @@ def test_bmi():
 def test_boer():
     assert leanbodymass.boer(180, 60, "m") == 53.3
 
+    with pytest.raises(ValueError):
+        leanbodymass.boer(180, 60, "g")
+
 
 def test_hume66():
     assert leanbodymass.hume66(180, 60, "m") == 51.2
+    with pytest.raises(ValueError):
+        leanbodymass.hume66(180, 60, "g")
 
 
 def test_hume71():
-    pass
+    with pytest.raises(ValueError):
+        leanbodymass.hume71(180, 60, "g")
 
 
 def test_janmahasation():
     assert leanbodymass.janmahasation(180, 60, "m") == 52.1
     assert leanbodymass.janmahasation(165, 90, "f") == 49.5
+
+    with pytest.raises(ValueError):
+        leanbodymass.janmahasation(180, 60, "g")
