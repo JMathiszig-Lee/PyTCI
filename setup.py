010b2f6c09912e929f45a3ce41b9f768a66ed4f7
Jakob Mathiszig-Lee <jakob@mathisziglee.co.uk>
2019-02-16 13:56:24 +0000

    starting to package things up!

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyTCI",
+    version="0.0.1",
+    author="Jakob Mathiszig-Lee",
+    author_email="jakob@mathisziglee.co.uk",
+    description="A package for target controlled infusions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JMathiszig-Lee/PyTCI",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
