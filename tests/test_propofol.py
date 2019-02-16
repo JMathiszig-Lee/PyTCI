cdab92f93fc9a00fa6ef24e19deba9c23e2c2d5b
Jakob Mathiszig-Lee <jakob@mathisziglee.co.uk>
2019-02-16 12:45:00 +0000

    added marsh and removed superflous files

--- tests/test_propofol.py
+++ tests/test_propofol.py
@@ -1,4 +1,5 @@
 from ..models import propofol
+#source for values independantly derived https://academic.oup.com/view-large/91165989
 
 def test_schnider():
     testpatient = propofol.Schnider(40, 70, 170, 'm')
@@ -9,3 +10,10 @@ def test_schnider():
 
     testpatient.give_drug(200)
     assert round(testpatient.x1, 2) == 46.84
+
+def test_marsh():
+    testpatient = propofol.Marsh(70)
+
+    assert round(testpatient.v1, 1) == 16.0
+    assert round(testpatient.v2, 1) == 32.4
+    assert round(testpatient.v3, 1) == 202.5
